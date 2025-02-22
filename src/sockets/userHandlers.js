1a2b0d461c7c403d45646028a066970975bdb995
felduque557 <jf.duque557@gmail.com>
2025-02-22 18:19:04 -0500

    Limpieza de logs y codigo innecesario

--- src/sockets/userHandlers.js
+++ src/sockets/userHandlers.js
@@ -3,12 +3,11 @@ const users = new Set();
 
 export function setupUserHandlers(socket, io) {
   const userId = `User${Math.floor(Math.random() * 1000)}`;
+  users.add(userId);
   io.emit("updateUsers", Array.from(users));
 
   socket.on("disconnect", () => {
     users.delete(userId);
-    console.log(userId);
-    console.log(Array.from(users));
     io.emit("updateUsers", Array.from(users));
   });
 
