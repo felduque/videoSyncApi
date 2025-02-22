67de7d4fcea94feee3716e0f18185e0789eeb44e
felduque557 <jf.duque557@gmail.com>
2025-02-22 18:10:34 -0500

    puerto por env o 3001 como opcional

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import server from "./src/app.js";
 
 const port = process.env.PORT || 3001;
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server runnin on ${process.env.PORT}`);
+server.listen(port, () => {
+  console.log(`Server runnin on ${port}`);
 });
